8bebe8a50bc8d091a30bc173b4bb6b61ed19c2d2
franco <pfranap@gmail.com>
2016-12-20 00:51:07 -0500

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,7 +95,6 @@ if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
     # Running on production App Engine, so use a Google Cloud SQL database.
     DATABASES = {
         'default': {
-            
             'ENGINE': 'django.db.backends.mysql',
             'HOST': '/cloudsql/duper-150604:duperdb',
             'NAME': 'django_test_db',
