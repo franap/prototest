3bce6ed4108d8f0fa1e63a63612b7d123cd6e65c
franco <pfranap@gmail.com>
2016-12-20 00:49:53 -0500

    Cambio en settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,6 +95,7 @@ if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
     # Running on production App Engine, so use a Google Cloud SQL database.
     DATABASES = {
         'default': {
+            
             'ENGINE': 'django.db.backends.mysql',
             'HOST': '/cloudsql/duper-150604:duperdb',
             'NAME': 'django_test_db',
